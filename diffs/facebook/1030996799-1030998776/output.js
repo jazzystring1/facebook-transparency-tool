bd22574478a244091a0a849a343ae26e5a02af4b
jazzystring1 <33471627+jazzystring1@users.noreply.github.com>
2025-12-12 10:13:31 +0000

    Processed revision: product=facebook, version=1030998776

---
+++ diffs/facebook/1030996799-1030998776/output.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "added": {},
+  "removed": {},
+  "modified": {
+    "LWIInstantAdsGoalPickerModalQuery_facebookRelayOperation": {
+      "old": "8e79d7021b2b636f840c7ff8ad962d6e95aeb30fd10134993148197802a2983e",
+      "new": "ad6ab978dcadf374fede13e5be38e1df9f99504ee6cfb5eb3bf117a69ed5276a"
+    },
+    "LWIInstantAdsRootQuery_facebookRelayOperation": {
+      "old": "0dba86fd846d90eede5c3bc9e90efa50eecdfeb2ced3a3f1d842856e79e8c051",
+      "new": "f7c78e3635a573bc21aaf90abf5e2ceb72a17d4c3ac3fc9d86ffb793ee103dc0"
+    }
+  }
+};
