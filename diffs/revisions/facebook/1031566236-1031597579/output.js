bd5b7feb8a6c4542a0c95939137da4126218ef28
jazzystring1 <33471627+jazzystring1@users.noreply.github.com>
2026-01-03 07:00:05 +0000

    Processed revision: product=facebook, version=1031597579

---
+++ diffs/revisions/facebook/1031566236-1031597579/output.js
@@ -0,0 +1,106 @@
+module.exports = {
+  "added": {
+    "XAdsManagerAutomatedResponseSuggestionControllerRouteBuilder": "4d2c6da57ea0c68ea7a788027c6f99cf8da750ac2712d79568b1b9fce977ce5a",
+    "XBoostedComponentDialogChangeControllerRouteBuilder": "6da36c5afa2268b48b656056a75cd9a08a392df9b8799e1e37e675732fb748f9",
+    "XBoostedComponentDialogSubmissionControllerRouteBuilder": "5fa4bc3dd04220d4986960067ca75954aefa844143f15c2c3ce625fb614c927d",
+    "XBoostedComponentFetchDialogDataControllerRouteBuilder": "e2c8b604df466b0b3c57841ca64203c8304a740077950c4d70bd2e05c0339685",
+    "XAdsGuidanceFeedbackActionAsyncControllerRouteBuilder": "dd89b60e6fc16cf8a68881c01aa20ee72027c20bed24c7acf19f5aa8bf00cfab",
+    "XAdPreviewConsistencySampleControllerRouteBuilder": "cc4bf11cc6853aff8084f6f9a88b7444f675961398357805773e22b587da20da",
+    "CometCreatorComposerPostingPagePreloadWrapperQuery_facebookRelayOperation": "b6279020d81230fddb65dd84a16d870c99698a6a9af752c45ceed068bf12f66d",
+    "ShareToChannelsCometComposerPushPageWrapperQuery_facebookRelayOperation": "52944dfca1528de168536ceadd3d076d1c459c52f245cd66a3a633b676c3d878",
+    "CometCreatorComposerPostingPageLazyloadWrapperQuery_facebookRelayOperation": "bdb8af0e7ba1c1b929db4a9b38c3670722dfe202ec032920dad5f325fa654294",
+    "CometCreatorComposerMonetizationToolsPageWrapperQuery_facebookRelayOperation": "c5b9f70ba562f527b8e4a78955a2116e6896bcefff2093a146e69b816d8b2398"
+  },
+  "removed": {
+    "XBizWebCometBizSuiteSettingsPagesControllerRouteBuilder": "d4a99c73f92835fd2e2712e86718e18a921dba9cc80d007619dbdd9c6cc2ca1a",
+    "XFBSbxGenAI3PImageViewerControllerRouteBuilder": "c78089ebb6efa8d05139df1a12b3bdbbcb1992b8b126b09c7d9dfc546ce5a4f4",
+    "CometCreatorComposerAllOptionsQuery_facebookRelayOperation": "a218f1bef809f45fd96193cc02c35e0bb24738c18a718c8e6d29e4d7bb748934",
+    "useWorkroomsNavToCreateGenesisWorkAccount_generateLinkMutation_facebookRelayOperation": "6d71720ac0caa134c5245b3816bc4112c40c84fd995a0e5f4ca0a261e3728e29",
+    "ReelComposerMusicChartInfoDialogQuery_facebookRelayOperation": "27fce04da6128d2a38295fb9cafb1f28cde6c0cdb609ed92711f5221e1e46c59",
+    "XBoostedComponentDialogChangeController": "d3ae218d5490b5e05631d075ccb1e9217b28d36dc40ffde57cf77952588de990",
+    "XBoostedComponentDialogSubmissionController": "17a5e5e7e2178eb5b63b372c81213583f4c039ee2b2cee7dc56311f233c6e6b0",
+    "XBoostedComponentFetchDialogDataController": "910c9ccdfc17c9f4e54d63d893f41438905fc2334a1fe3b22dcb3a96a5bba0e9",
+    "XPagesManagerInsightsController": "865b33c7ed32ee7da463b473c3f2096c26f7836a14b39ab7fc7ac15060e53836",
+    "XAdsManagerAutomatedResponseSuggestionController": "3406f44450ad56d5660ff40d7b4df3db3927eb55e68dc3effae3e29ccdd28feb",
+    "XAdsGuidanceFeedbackActionAsyncController": "7914b80f733711f460790c5a2da1b6ebf6755158c54833f0ae2cecc2eedce656",
+    "XAdPreviewConsistencySampleController": "15d1cf816dddf506828b6589691495de351dde8661499fcdcfca08cfb4955c15"
+  },
+  "modified": {
+    "useCurrentRouteBuilder": {
+      "old": "0fb8817e91b723c627659a448671a373ac81e23fcf42ed14bffcceb2f2da0415",
+      "new": "caf95e3dcafbce579a20ce2af84a3211aab8308aec7f81cf84fec0289827dff4"
+    },
+    "FBECometAppStorePermissionsView.react": {
+      "old": "3a79903bc9c8627e407779aaf4604b4c25374fa8c12f287f400a062fc9080169",
+      "new": "6db67a80913784d66ca8847543ed3f6d65364bdc5f8955766bc442589d35c779"
+    },
+    "CometAuthenticitySecurityCardBody.react": {
+      "old": "17016d3ae5cd1a178bca788a028e90a30b6b50ebaa0c99f1ed0872c2ebf528f9",
+      "new": "d1aeffed913088dfb76eb6d209b3638ad534a234e3645b2ac4e3ba640ec20465"
+    },
+    "IGDCreateSlideMessageUpdateQuery_instagramRelayOperation": {
+      "old": "254be2795148d5b124513bfa2a86f53f574f6315badf25f25078f3556a0b9cd5",
+      "new": "76c16aa74a5cbc0a8d5859365ca61fa59604a9a34dc6354fd659321853875546"
+    },
+    "useFBGenAIResponseCardStreamMutation_facebookRelayOperation": {
+      "old": "30eb52770245f3021c750811107500f9ed3f6f52013bd5735cab13d12111ae6a",
+      "new": "911589eaa32af598f6aaf43bf118a28177de9517f8ce21ade8e427fbe4f29541"
+    },
+    "ProfilePlusCometGlobalPagesSettingsLayoutQuery_facebookRelayOperation": {
+      "old": "d891cf6113bb35ef0e5135cd90aa2db45c985cad8ed2bceed94a4c61ec83802f",
+      "new": "b891b050d5151b09a2efcaedf9a995f1847d3d8d3df46dda1ab25f69043cbcb6"
+    },
+    "CometProfilePlusAdminPermissionsRootQuery_facebookRelayOperation": {
+      "old": "81dd0fe8e573804ab059803433556e5e1b9136758c74c0ef8ff75491901fd5e9",
+      "new": "2ebf8e4e8f595fd1bc790792d9ee01809445e6729783ccfc97c9cd1ea3517b8c"
+    },
+    "SearchCometInterestsDeepDivePostsQuery_facebookRelayOperation": {
+      "old": "fcbdbfc30e4a43c39a791411091cc8d43b8881e9f69d06c7b48f70ff8223949c",
+      "new": "38156935a27373583c19ef38d3734cdbe78cc4c4fefff16468b532606361d173"
+    },
+    "SearchCometInterestsDeepDiveRootContentQuery_facebookRelayOperation": {
+      "old": "233d00fdb6b329bd9a826ee66e630f719bc4db90f4b264d41637e631a8314c95",
+      "new": "258299ea57d544cd99a821d44b5d5caca8aa955d0ec40c280eef8557b5269146"
+    },
+    "ReelsComposerShareToChannelsQuery_facebookRelayOperation": {
+      "old": "ae52b771e05cc3dcb4d7926f8316cf7b919aa069d453c5645e2d19acbbe4afb9",
+      "new": "1c62c202a01fbb712fe300787574c7d9bc7c0784f462ec5d27c460174af7dbcf"
+    },
+    "ProfileCometMentionsFeedQuery_facebookRelayOperation": {
+      "old": "d18e4bbd00f2d31df9960040f24cbb52e25c99148c211710bc9437eefd122f4a",
+      "new": "a9f40be91ef10198dfb7342e18efb70f82017de2c92323d944561c59002100c6"
+    },
+    "ProfileCometMentionsTabWithDeepDiveRootQuery_facebookRelayOperation": {
+      "old": "db99aa474567eb8589d58d88084d154bf4b18cdb292460c495b0f34f10ab4b16",
+      "new": "0b7bff3ab59214c44d0a0ede3007d0652664077dde1501f13d8575430105b1c9"
+    },
+    "CometVideoHomeHashtagRootQuery_facebookRelayOperation": {
+      "old": "b32f512bf62bee70c1d5449719116a7b60813701c8d652c4f8e41b163da35869",
+      "new": "ce7ea57d69aeec0163e03ea52d244bfac91661ab4d5448fa5832116c1c39ef18"
+    },
+    "useGlobalPagesEditExampleCountriesMutation_facebookRelayOperation": {
+      "old": "e7e30e30ee3e226d2639494293e75249636376e5d954507682c09f96b9444943",
+      "new": "baf81c436175df4a29ad17cb14c213f03e0cffe39fa60d5bcfbc837feccb6e0c"
+    },
+    "ProfileCometLoggedOutRootQuery_facebookRelayOperation": {
+      "old": "339989a4c60cb25aeff5f4a92cc5627100f23e8af42776265100c303c15744a0",
+      "new": "565ce2c6616c7202345096286f60eaf45d83cae8bd486061cb9bedc44c2e2b78"
+    },
+    "useGlobalPagesChangeDefaultPageMutation_facebookRelayOperation": {
+      "old": "c92e603eacdeb0066e364c6b79e090d03153bd4f811f7b8cd31d5f7ecee8e487",
+      "new": "ee73302443a8b611bd1b53f3fddb7b3594280364386e46d41e525f65af46cbb8"
+    },
+    "FBWebMetaAIAnswerSheetPermalinkDialogRootQuery_facebookRelayOperation": {
+      "old": "873c602f0064e45c87e3c42409031098bcad1e7b96e94d6eef2922b522cb7d19",
+      "new": "c3285f18d9d4c7f60d9ae8600d43638d0633633e3168a21dea51354b0bd9f748"
+    },
+    "useGlobalPagesAddMarketPageMutation_facebookRelayOperation": {
+      "old": "caddd2faa7f5f698171bad78985b027ca05122ca4c62d869ef55ae40bacb0529",
+      "new": "6338bbba8b7be5b98cb40394b1000ce22ba8ba7368e2b5fc83c3c9cd89847487"
+    },
+    "useGlobalPagesUpdateMarketPageTargetingMutation_facebookRelayOperation": {
+      "old": "9207e27bc211a93eb57227e0e136ea868edc509f19f2430f37b9307ed7e05626",
+      "new": "77665a4d3446bdfce498cda668e5ac2b04c2af87937b99887c45462899ee4506"
+    }
+  }
+};
