73adf31623aa361c0f70933fac6d3d0e7a55b9ed
jazzystring1 <33471627+jazzystring1@users.noreply.github.com>
2026-01-04 12:26:19 +0000

    Processed revision: product=facebook, version=1031612156

---
+++ diffs/revisions/facebook/1031609333-1031612156/output.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "added": {},
+  "removed": {},
+  "modified": {
+    "FXFetaMigrationMomentProfilesDialogQuery_threadsRelayOperation": {
+      "old": "cdf36dee8ee425496fc5c5583c850e949574677397ea403892a90e26d0494114",
+      "new": "7308b0efea6dcc7f9ef704c7a9e8068e7ca1b919f0a856f5f4eca6e656814033"
+    },
+    "FXFetaMigrationMomentProfilesDialogQuery_facebookRelayOperation": {
+      "old": "cdf36dee8ee425496fc5c5583c850e949574677397ea403892a90e26d0494114",
+      "new": "7308b0efea6dcc7f9ef704c7a9e8068e7ca1b919f0a856f5f4eca6e656814033"
+    },
+    "FXFetaMigrationMomentProfilesDialogQuery_instagramRelayOperation": {
+      "old": "cdf36dee8ee425496fc5c5583c850e949574677397ea403892a90e26d0494114",
+      "new": "7308b0efea6dcc7f9ef704c7a9e8068e7ca1b919f0a856f5f4eca6e656814033"
+    }
+  }
+};
